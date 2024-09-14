ebe56b5668e90f2916eccb1232c3ac7e7d73ffc4
Eunice <90015739+xxceanne@users.noreply.github.com>
2024-09-14 17:23:59 -0600

    added CSS animations

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+	const observer = new IntersectionObserver(entries => {
+	  entries.forEach(entry => {
+		if (entry.isIntersecting) {
+		  entry.target.classList.add('in-view');
+		  return;
+		}
+		entry.target.classList.remove('in-view');
+	  });
+	});
+
+	const allAnimatedElements = document.querySelectorAll('.ser-animated, .solutions-animated');
+
+	allAnimatedElements.forEach((element) => observer.observe(element));
+
+}); 
